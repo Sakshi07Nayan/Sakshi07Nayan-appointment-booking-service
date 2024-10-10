a72b87bd0138a5da7ef652fcbd7e44e614464395
Sakshi Nayan <sakshi12princi@gmail.com>
2024-10-10 15:09:43 +0530

    get route and some pack.. installation

--- server-app/server.js
+++ server-app/server.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 // Database connection
 const pool = new Pool({
