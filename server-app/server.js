8b0e9c5e19498da5669f14cbb65a702b6b32aa96
Sakshi Nayan <sakshi12princi@gmail.com>
2024-10-13 20:27:06 +0530

    cores changes

--- server-app/server.js
+++ server-app/server.js
@@ -63,28 +63,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
-// import { fileURLToPath } from 'url';
-// import path from 'path';
-
-// const _filename = fileURLToPath(import.meta.url);
-// const _dirname = path.dirname(_filename);
-
-
 const app = express();
 const port = process.env.PORT || 5000;
-
-//user the client app
-// app.use(express.static(path.join(_dirname,'/client/build')));
-// app.get('*',(req,res)=> res.sendFile(path.join(_dirname,'/client/build/index.html')))
-
-// Database connection using environment variables
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT || 5432,
-//     database: process.env.DB_NAME
-// });
 
 //local database
 // const pool = new Pool({
@@ -112,7 +92,12 @@ pool.connect((err, client, release) => {
 });
 
 
-app.use(cors({ origin: '*' }));
+// app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: 'https://appointment-booking-service-ogzq.vercel.app', // Replace with your React app domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true, 
+}));
 app.use(express.json());
 
 // Endpoint to book an appointment
@@ -122,11 +107,11 @@ app.post('/api/appointments', async (req, res) => {
     console.log('Received data:', { name, email, date, service });
 
     // Convert date from dd-mm-yyyy to yyyy-mm-dd
-    const [day, month, year] = date.split('-');
-    const formattedDate = `${year}-${month}-${day}`; // Convert to yyyy-mm-dd format
+    // const [day, month, year] = date.split('-');
+    // const formattedDate = `${year}-${month}-${day}`; // Convert to yyyy-mm-dd format
 
     const query = 'INSERT INTO appointments (name, email, date, service) VALUES ($1, $2, $3, $4) RETURNING *';
-    const values = [name, email, formattedDate, service];
+    const values = [name, email, date, service];
 
     const result = await pool.query(query, values);
     res.status(201).json(result.rows[0]);
